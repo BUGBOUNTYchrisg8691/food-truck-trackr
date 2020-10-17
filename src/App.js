d74ae111b89e41714e9d1699daf9dd64edf19fec
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-10-17 00:55:31 -0400

    init commit/header started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import 'fontsource-roboto';
+
+import Header from "./components/Header/Header"
+import SignIn from "./components/SignIn/SignIn"
+import SignUp from "./components/SignUp/SignUp"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />      
+
+      <Switch>
+        <Route path="/login/signup">
+          <SignUp />
+        </Route>
+        <Route path="/login/signin">
+          <SignIn />
+        </Route>
+      </Switch> 
+    </>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Switch, Route, NavLink } from "react-router-dom"
+
+import "./Header.css"
+
+import headerBanner from "../../assets/headerImg.png"
+
+import SignUp from "../SignUp/SignUp"
+import SignIn from "../SignIn/SignIn"
+
+export default function Login() {
+  return (
+    <div className="header-container">
+      <img src={headerBanner} href="food truck" />
+      <div className="nav-header">
+        <NavLink
+          activeClassName="active"
+          className="nav-link left"
+          to="/login/signup"
+        >Sign Up</NavLink>
+        <NavLink
+          activeClassName="active"
+          className="nav-link right"
+          to="/login/signin"
+        >Sign In</NavLink>
+      </div>
+    </div>
+  )
+}
